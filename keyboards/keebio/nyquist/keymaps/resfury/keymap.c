0bb725672bbd2fd9891378f297cfe4bc1e2f518c
ResoluteFury <aaron@resfury.com>
2023-02-23 11:28:28 -0500

    Add debounce time, swap volume controls, mirror top row on second row for mods

--- keyboards/keebio/nyquist/keymaps/resfury/keymap.c
+++ keyboards/keebio/nyquist/keymaps/resfury/keymap.c
@@ -101,7 +101,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Esc  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
  * ,-----------------------------------------------------------------------------------.
- * |      |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  |      |
+ * |      |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |Enter |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -112,17 +112,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT( \
   KC_ESC,  KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,   KC_ASTR, KC_LPRN, KC_RPRN,       KC_DEL,  \
-  _______, KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,   KC_ASTR, KC_LPRN, KC_RPRN,       _______, \
+  _______, KC_EXLM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,   KC_ASTR, KC_LPRN, KC_RPRN,       KC_DEL,  \
   _______, KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,   KC_PLUS, KC_LCBR, KC_RCBR,       _______, \
   _______, KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_GRV), KC_GRV,  KC_MUTE, RCTL(KC_BSLS), KC_PIPE, \
-  _______, _______,  _______, _______, _______, KC_BSPC, KC_SPC,  ADJUST,    KC_MPLY, KC_VOLU, KC_VOLD,       KC_MNXT  \
+  _______, _______,  _______, _______, _______, KC_BSPC, KC_SPC,  ADJUST,    KC_MPLY, KC_VOLD, KC_VOLU,       KC_MNXT  \
 ),
 
 /* Raise
  * ,-----------------------------------------------------------------------------------.
  * | Esc  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
  * ,-----------------------------------------------------------------------------------.
- * |      |   1  |   2  |   3  |      |      |      |      |      |      |      |      |
+ * |      |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |   4  |   5  |   6  |   +  |   -  |  +   |   -  |   =  |   [  |   ]  |Enter |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
@@ -133,10 +133,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT( \
   KC_ESC,  KC_1,    KC_2,  KC_3,   KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,          KC_DEL,  \
-  _______, KC_1,    KC_2,  KC_3,   _______, _______, _______, _______, _______, _______, _______,       _______, \
+  _______, KC_1,    KC_2,  KC_3,   KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,          KC_DEL,  \
   _______, KC_4,    KC_5,  KC_6,   KC_PLUS, KC_MINS, KC_PLUS, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC,       _______, \
   _______, KC_7,    KC_8,  KC_9,   KC_ASTR, KC_SLSH, KC_ASTR, KC_SLSH, KC_DOT,  KC_MUTE, RCTL(KC_BSLS), KC_BSLS, \
-  _______, KC_COMM, KC_0,  KC_DOT, ADJUST,  KC_BSPC, KC_SPC,  _______, KC_MPLY, KC_VOLU, KC_VOLD,       KC_MNXT  \
+  _______, KC_COMM, KC_0,  KC_DOT, ADJUST,  KC_BSPC, KC_SPC,  _______, KC_MPLY, KC_VOLD, KC_VOLU,       KC_MNXT  \
 ),
 
 /* Adjust (Lower + Raise)
@@ -166,7 +166,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |      |      |      |      |      |      | S(Hm)| Home |  Up  | End  |S(End)|      |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      | Ctrl | Shift| Alt  |      |      |      | Left | Down |Right |      |      |
+ * |      | Ctrl | Shift| Alt  |      |      | PgDn | Left | Down |Right | PgUp |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -176,7 +176,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_FUNCTION] = LAYOUT( \
   KC_CAPS, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______,   _______, \
   _______, _______, _______, _______, _______, _______, S(KC_HOME), KC_HOME, KC_UP,   KC_END,  S(KC_END), _______, \
-  _______, KC_LCTL, KC_LSFT, KC_LALT, _______, _______, _______,    KC_LEFT, KC_DOWN, KC_RGHT, _______,   _______, \
+  _______, KC_LCTL, KC_LSFT, KC_LALT, _______, _______, KC_PGDN,    KC_LEFT, KC_DOWN, KC_RGHT, KC_PGUP,   _______, \
   _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______,   _______, \
   _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______,   _______  \
 )
